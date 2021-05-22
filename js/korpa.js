73f56a2be0ba8aad2954a7248ae3ce94772582ae
eisic2 <eisic2@etf.unsa.ba>
2021-05-22 22:00:19 +0200

    dodana kropa

--- js/korpa.js
+++ js/korpa.js
@@ -1,34 +1,186 @@
-//onsole.log(sadrzajRacuna.innerHTML + "xxxxx");
-//sadrzajRacuna.innerHTML = "";
-function sadrzaj(proizvod, kolicina, cijena){
-	var tabela = '<table><tr id="naslovi"><td id="1">NAZIV</td><td id="2">KOLIČINA</td><td id="3">CIJENA</td></tr>';
+var shoppingCart = (function() {
 
-	tabela += "<tr><td>" + proizvod + "</td><td>" + kolicina + "</td><td>" + cijena + "</td></tr>";
-	console.log(tabela);
-		/*cijene.push(x);
-		proizvodi.push(naziv);
-		kolicine.push(kolicina);*/
-		//console.log("Ovo su proizvodi: " + proizvodi);
-		//console.log("Ovo je sadržaj html-a: " + sadrzajRacuna);
-		
-
-		//string += naziv + "  " + kolicina + "  " + cijena + "<br>";
-
-	
-	tabela += "</table>";
-
-	document.getElementById('racun').innerHTML = tabela;	
-}
-
-
-//sadrzajRacuna.innerHTML = proizvodi.toString();
-//doc.getElementsByClassName('racun').innerHTML = string;
-
-/*function nacrtajTabelu(podaci,error){
-    if(error==null || error==undefined){
-        
-        for(var i=0;i<podaci.length;i++){
-            tabela+="<tr><td>" + podaci[i].naziv + "</td><td>" + podaci[i].aktivnost + "</td><td>" + podaci[i].dan + "</td><td>" + podaci[i].start + "</td><td>" +podaci[i].end +"</tr>"  ;
+    cart = [];
+    
+    // Konstruktor
+    function Item(name, price, count) {
+      this.name = name;
+      this.price = price;
+      this.count = count;
+    }
+    
+    // spremi korpu
+    function saveCart() {
+      sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
+    }
+    
+      // ucitavanje korpe
+    function loadCart() {
+      cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
+    }
+    if (sessionStorage.getItem("shoppingCart") != null) {
+      loadCart();
+    }
+    
+  
+    var obj = {};
+    
+    // Dodaj u korpu
+    obj.addItemToCart = function(name, price, count) {
+      for(var item in cart) {
+        if(cart[item].name === name) {
+          cart[item].count ++;
+          saveCart();
+          return;
         }
+      }
+      var item = new Item(name, price, count);
+      cart.push(item);
+      saveCart();
+    }
 
-*/
+    // postavi broj od stavke
+    obj.setCountForItem = function(name, count) {
+      for(var i in cart) {
+        if (cart[i].name === name) {
+          cart[i].count = count;
+          break;
+        }
+      }
+    };
+
+    // ukloniti predmet iz korpe
+    obj.removeItemFromCart = function(name) {
+        for(var item in cart) {
+          if(cart[item].name === name) {
+            cart[item].count --;
+            if(cart[item].count === 0) {
+              cart.splice(item, 1);
+            }
+            break;
+          }
+      }
+      saveCart();
+    }
+  
+    // ukloniti sve predmete iz korpe
+    obj.removeItemFromCartAll = function(name) {
+      for(var item in cart) {
+        if(cart[item].name === name) {
+          cart.splice(item, 1);
+          break;
+        }
+      }
+      saveCart();
+    }
+  
+    // ocisti korpu
+    obj.clearCart = function() {
+      cart = [];
+      saveCart();
+    }
+  
+    // broji artikle 
+    obj.totalCount = function() {
+      var totalCount = 0;
+      for(var item in cart) {
+        totalCount += cart[item].count;
+      }
+      return totalCount;
+    }
+  
+    // Total 
+    obj.totalCart = function() {
+      var totalCart = 0;
+      for(var item in cart) {
+        totalCart += cart[item].price * cart[item].count;
+      }
+      return Number(totalCart.toFixed(2));
+    }
+  
+    // Lista korpe
+    obj.listCart = function() {
+      var cartCopy = [];
+      for(i in cart) {
+        item = cart[i];
+        itemCopy = {};
+        for(p in item) {
+          itemCopy[p] = item[p];
+  
+        }
+        itemCopy.total = Number(item.price * item.count).toFixed(2);
+        cartCopy.push(itemCopy)
+      }
+      return cartCopy;
+    }
+
+    return obj;
+  })();
+
+
+  // Dodavanje stavke
+  $('.buynow-btn').click(function(event) {
+    event.preventDefault();
+    var name = $(this).data('name');
+    var price = Number($(this).data('price'));
+    shoppingCart.addItemToCart(name, price, 1);
+    displayCart();
+  });
+  
+  // Obrisi stavke
+  $('.clear-cart').click(function() {
+    shoppingCart.clearCart();
+    displayCart();
+  });
+  
+  
+  function displayCart() {
+    var cartArray = shoppingCart.listCart();
+    var output = "";
+    for(var i in cartArray) {
+      output += "<tr>"
+        + "<td>" + cartArray[i].name + "</td>" 
+        + "<td>(" + cartArray[i].price + ")</td>"
+        + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
+        + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
+        + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
+        + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
+        + " = " 
+        + "<td>" + cartArray[i].total + "</td>" 
+        +  "</tr>";
+    }
+    $('.show-cart').html(output);
+    $('.total-cart').html(shoppingCart.totalCart());
+    $('.total-count').html(shoppingCart.totalCount());
+  }
+  
+  // Dugme Izbriši stavku
+  $('.show-cart').on("click", ".delete-item", function(event) {
+    var name = $(this).data('name')
+    shoppingCart.removeItemFromCartAll(name);
+    displayCart();
+  })
+  
+  // -1
+  $('.show-cart').on("click", ".minus-item", function(event) {
+    var name = $(this).data('name')
+    shoppingCart.removeItemFromCart(name);
+    displayCart();
+  })
+
+  // +1
+  $('.show-cart').on("click", ".plus-item", function(event) {
+    var name = $(this).data('name')
+    shoppingCart.addItemToCart(name);
+    displayCart();
+  })
+  
+  // Unos broja stavki
+  $('.show-cart').on("change", ".item-count", function(event) {
+     var name = $(this).data('name');
+     var count = Number($(this).val());
+    shoppingCart.setCountForItem(name, count);
+    displayCart();
+  });
+  
+  displayCart();
